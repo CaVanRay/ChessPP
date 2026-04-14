73f16640fd5ad35c82859453d693b28b39d6e2fd
Cavan Theiss <cavanrt@gmail.com>
2026-04-14 13:19:00 -0500

    Create basicBoard.cpp

---
+++ basicBoard.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+class piece {
+
+};
+
+struct Node {
+
+    Node* up;
+    Node* down;
+    Node* prev;
+    Node* next;
+    int column;
+    char row;
+    bool occupied;
+    piece* occupant;
+
+};
+
+void horizontal(const int& edges) {
+    std::cout << "+";
+    for (int i = 0; i < edges; i++) {
+        std::cout << "-----+";
+    }
+    std::cout << std::endl;
+}
+
+void vertical(const int& spaces) {
+    std::cout << "|";
+    for (int i = 0; i < spaces; i++) {
+        std::cout << "     |";
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+    char cont = 'Y';
+    int boardSize = 8;
+    do {
+        std::cout << "What size board? :";
+        std::cin >> boardSize;
+        std::cout << std::endl << std::endl;
+        horizontal(boardSize);
+        for (int s = 0; s < boardSize; s++) {
+            for (int t = 0; t < 3; t++) {
+                vertical(boardSize);
+            }
+            horizontal(boardSize);
+        }
+        std::cout << std::endl << std::endl;
+        std::cout << "Would you like another board? (Y/N)";
+        std::cin >> cont;
+    } while (cont == 'Y');
+
+    return 0;
+}
