bf59d128cf6cf5c9a7b336618a11e2593cccb46a
Cavan Theiss <cavanrt@gmail.com>
2026-04-18 17:23:55 -0500

    Update basicBoard.cpp

--- basicBoard.cpp
+++ basicBoard.cpp
@@ -19,18 +19,10 @@ void horizontalOutlines(const int& edges) {
     std::cout << std::endl;
 }
 
-void spaces(const int& spaces, const int& column, const int& row) {
+void spaces(const int& spaces) {
     std::cout << "|";
     for (int i = 0; i < spaces; i++) {
-        if (column == 0 && row == 1) {
-            std::cout << "  X  |";
-        }
-        else if (column == 7 && row == 1) {
-            std::cout << "  O  |";
-        }
-        else {
-            std::cout << "     |";
-        }
+
     }
     std::cout << std::endl;
 }
@@ -43,7 +35,7 @@ int main()
         horizontalOutlines(boardSize);
         for (int s = 0; s < boardSize; s++) {
             for (int t = 0; t < 3; t++) {
-                spaces(boardSize, s, t);
+                spaces(boardSize);
             }
             horizontalOutlines(boardSize);
         }
