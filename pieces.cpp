746178380d8d155cc71718f4ee4e4a1c3870b392
Cavan Theiss <cavanrt@gmail.com>
2026-08-29 20:12:24 -0500

    Update pieces.cpp

--- pieces.cpp
+++ pieces.cpp
@@ -7,7 +7,10 @@ just a collection of classes for chess pieces.
 more for practicing classes than anything else.
 
 things to track:
+
 row & column - current position
+possible movements
+
 
 *****************************************************/
 class piece {
