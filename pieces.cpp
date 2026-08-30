ea8b0f9e07f08da04ded1ab13bdaad0219ea7495
Cavan Theiss <cavanrt@gmail.com>
2026-08-29 20:06:00 -0500

    Update pieces.cpp

--- pieces.cpp
+++ pieces.cpp
@@ -3,6 +3,8 @@ Date: 08/29/2026
 Title: pieces.cpp
 Author: Cavan Ray Theiss
 Description:
+just a collection of classes for chess pieces.
+more for practicing classes than anything else.
 
 *****************************************************/
 class piece {
