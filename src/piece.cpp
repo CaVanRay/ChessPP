248a2c954bc9c964c4c1c7e6e34453f0987e97bf
Cavan Theiss <cavanrt@gmail.com>
2026-08-30 11:46:07 -0500

    Update piece.cpp

--- src/piece.cpp
+++ src/piece.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
 
+class piece {
+public:
+    int xCoordinate, yCoordinate;
+    std::string color;
+
+    virtual bool validMoveCheck(int& possX, int& possY) = 0;
+
+};
