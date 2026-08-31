9c57feb5ae51b14fcabedd1ca455c732af2c0894
Cavan Theiss <cavanrt@gmail.com>
2026-08-31 10:04:20 -0500

    Update bishop.cpp

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -4,6 +4,12 @@
 
 class bishop : public piece {
 public:
-    bool validMoveCheck(int& possX, int& possY) override {}
+    bool validMoveCheck(int& possX, int& possY) override {
+        if(std::abs(possX - xCoordinate) == std::abs(possY - yCoordinate)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 };
