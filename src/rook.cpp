aa565de623ab185109e50c6d4b28570f03fc934b
Cavan Theiss <cavanrt@gmail.com>
2026-08-31 10:03:23 -0500

    Update rook.cpp

--- src/rook.cpp
+++ src/rook.cpp
@@ -6,7 +6,7 @@
 class rook : public piece {
 public:
     bool validMoveCheck(int& possX, int& possY) override {
-        if(std::abs(possX - xCoordinate) == std::abs(possY - yCoordinate)){
+        if((possX == xCoordinate)||(possY == yCoordinate)){
             return true;
         }else{
             return false;
