b52d61b8e469856baa19edf883ba5cffcfb9059e
Cavan Theiss <cavanrt@gmail.com>
2026-09-01 19:40:06 -0500

    Update bishop.cpp

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -2,10 +2,10 @@
 #include <cmath>
 
 
-    bool validMoveCheck(int& possX, int& possY) override {
-        if(std::abs(possX - xCoordinate) == std::abs(possY - yCoordinate)){
-            return true;
-        }else{
-            return false;
-        }
+bool validMoveCheck(int& possX, int& possY) override {
+    if(std::abs(possX - xCoordinate) == std::abs(possY - yCoordinate)){
+        return true;
+    }else{
+        return false;
     }
+}
