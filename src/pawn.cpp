331cb458c90f92fe0b35028e9684f1114101fb78
Cavan Theiss <cavanrt@gmail.com>
2026-09-02 09:54:34 -0500

    Update pawn.cpp

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -1,7 +1,3 @@
 #include "pawn.hpp"
 
-class pawn : public piece {
-public:
-    bool validMoveCheck(int& possX, int& possY) override {}
-
-};
+bool validMoveCheck(int& possX, int& possY) override {}
